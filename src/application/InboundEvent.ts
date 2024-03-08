6a873986275aadba4dba173a42dd6a763a86090d
Jonathan Eichenhofer <eichenhofer.jonathan@gmail.com>
2024-03-08 17:59:38 +0100

    feat: add props to inbound event

--- src/application/InboundEvent.ts
+++ src/application/InboundEvent.ts
@@ -1,11 +1,32 @@
+export interface InboundEventProperties<TPayload> {
+    streamName: string;
+    no: number;
+    id: string;
+    aggregateId: string;
+    aggregateVersion: number;
+    name: string;
+    payload: TPayload;
+    occurredAt: Date;
+}
+
 export class InboundEvent<TPayload> {
-    constructor(
-        readonly streamName: string,
-        readonly no: number,
-        readonly aggregateId: string,
-        readonly aggregateVersion: number,
-        readonly name: string,
-        readonly payload: TPayload,
-        readonly occurredAt: Date
-    ) {}
+    streamName: string;
+    no: number;
+    id: string;
+    aggregateId: string;
+    aggregateVersion: number;
+    name: string;
+    payload: TPayload;
+    occurredAt: Date;
+
+    constructor (readonly props: InboundEventProperties<TPayload>) {
+        this.streamName = props.streamName;
+        this.no = props.no;
+        this.id = props.id;
+        this.aggregateId = props.aggregateId;
+        this.aggregateVersion = props.aggregateVersion;
+        this.name = props.name;
+        this.payload = props.payload;
+        this.occurredAt = props.occurredAt;
+    }
 }
