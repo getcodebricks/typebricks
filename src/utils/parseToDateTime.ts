117faf7cc9c1f4c3394104497172e594b83a53a6
Jonathan Eichenhofer <eichenhofer.jonathan@gmail.com>
2024-09-30 13:16:24 +0200

    fix: parse to datetime to work on regex

--- src/utils/parseToDateTime.ts
+++ src/utils/parseToDateTime.ts
@@ -1,13 +1,13 @@
 export function parseToDateTime(key: any, value: any): any {
     if (typeof value === 'string') {
-        const date: Date = new Date(value);
 
-        return isValidDate(date) ? date : value;
+        return isValidDate(value) ? new Date(value) : value;
     }
     
     return value;
 }
 
-function isValidDate(date: Date): boolean {
-    return !isNaN(date.getTime());
+function isValidDate(dateTimeString: string): boolean {
+    const flexibleISO8601Regex = /^\d{4}-\d{2}-\d{2}(?:[T\s]\d{2}:\d{2}(?::\d{2}(?:\.\d+)?)?(?:Z|[+-]\d{2}:\d{2})?)?$/;
+    return flexibleISO8601Regex.test(dateTimeString);
 }
